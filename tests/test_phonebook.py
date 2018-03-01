5b2e3e73682d68c5d07b03a22fa9203719225680
Joshua Ondieki <joshuaondieki8@gmail.com>
2018-03-01 19:56:03 +0300

    Refactor test

--- tests/test_phonebook.py
+++ tests/test_phonebook.py
@@ -10,29 +10,31 @@ class TestPhoneBook(unittest.TestCase):
         self.instance.add('Joshua', 'Ondieki', '0700009999')
         self.instance.add('Andela', 'Kenya', '0788880000')
         self.instance.add('David', 'Kironde', '0790909090')
-        self.instance.add('extra', 'contact', '0711111111')
-        self.instance.add('outdated', 'name', '0720502050')
-        self.assertEqual(self.instance.data, 3, msg="Should indicate 3 contacts present in phone book!")
+        self.assertEqual(len(self.instance.data), 3, msg="Should indicate 3 contacts present in phone book!")
 
     def test_cannot_add_duplicates(self):
         self.instance.add('tobe', 'duplicated', '0700000000')
         response = self.instance.add('tobe', 'duplicated', '0700000000')
         self.assertEqual('Contact exists!', response, msg="Cannot create duplicate contact records")
 
-    def test_retrieves_contact(self):
-        self.assertTrue('Joshua' == self.instance.data['0700009999'][0] and self.instance.data['0700009999'][1], msg="Should retrieve contact and reflect the correct contact name!")
+    def test_retrieves_contacts(self):
+        self.instance.add('David', 'Kironde', '0790909090')
+        self.retrieved_contacts = self.instance.data
+        self.assertTrue('David' == self.retrieved_contacts['0790909090'][0] and "Kironde" == self.instance.data['0790909090'][1], msg="Should retrieve contact and reflect the correct contact name!")
 
     def test_delete_contact(self):
+        self.instance.add('extra', 'contact', '0711111111')
         self.instance.delete('0711111111')
         self.assertNotIn('0711111111', self.instance.data.keys(), msg="Deletes existing contact successfully!")
 
     def test_edit_contact(self):
+        self.instance.add('outdated', 'name', '0720502050')
         self.instance.edit('0720502050', 'updated', 'name')
         self.assertTrue('updated' == self.instance.data['0720502050'][0], msg="Contact info should be updated successfully!")
 
     def test_edit_works_only_for_existing_contacts(self):
         response = self.instance.edit('0704440666', 'edit', 'new')
-        self.assertTrue('No such contact!', response, msg="Only existing contacts can be edited!")
+        self.assertTrue('No such contact!' == response, msg="Only existing contacts can be edited!")
 
 
 if __name__ == '__main__':
